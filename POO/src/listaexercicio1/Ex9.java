583257f5c3864edc509cd55e386aa81cc93e8bb1
User <User@User-PC>
2018-08-18 22:56:36 -0300

    1808

---
+++ POO/src/listaexercicio1/Ex9.java
@@ -0,0 +1,32 @@
+package listaexercicio1;
+
+import java.util.Scanner;
+
+public class Ex9 {
+
+	public static void main(String[] args) {
+			System.out.print("Digite uma palavra: ");
+			Scanner tec = new Scanner(System.in);
+			String palavra = tec.next();
+			
+			for (int i = palavra.length() -1; i > -1; i--) {
+				System.out.print(palavra.charAt(i));
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+	}
+
+}
