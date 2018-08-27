ab58e2fe1669ad31f58682b2f1c51899fbbe0544
Aluno <Aluno@S211M28>
2018-08-27 11:09:55 -0300

    2708

---
+++ POO/src/exemploOO/Apartamento.java
@@ -0,0 +1,34 @@
+package exemploOO;
+
+public class Apartamento {
+
+	private double area;
+	private String cor;
+	private String nomeCondominio;
+	private int numero;
+	
+	public double getArea() {
+		return area;
+	}
+	public void setArea(double area) {
+		this.area = area;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getNome() {
+		return nomeCondominio;
+	}
+	public void setNome(String nome) {
+		this.nomeCondominio = nome;
+	}
+	public int getNumero() {
+		return numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+}
---
+++ POO/src/exemploOO/Bens.java
@@ -0,0 +1,49 @@
+package exemploOO;
+
+public class Bens {
+
+	private Carro carro;
+	private Moto moto;
+	private Casa casa;
+	private Apartamento apto;
+	
+	public Carro getCarro() {
+		return carro;
+	}
+	public void setCarro(Carro carro) {
+		this.carro = carro;
+	}
+	
+	public Moto getMoto() {
+		return moto;
+	}
+	public void setMoto(Moto moto) {
+		this.moto = moto;
+	}
+	
+	public Casa getCasa() {
+		return casa;
+	}
+	public void setCasa(Casa casa) {
+		this.casa = casa;
+	}
+	
+	public Apartamento getApto() {
+		return apto;
+	}
+	public void setApto(Apartamento apto) {
+		this.apto = apto;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
